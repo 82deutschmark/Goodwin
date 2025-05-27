cfcd0ee6440fa14b2de34782b94c659a688c55cb
82deutschmark <82deutschmark@gmail.com>
2025-05-26 22:37:03 -0400

    Vercel fix

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,10 @@
 export { default } from "next-auth/middleware";
 
-// Optionally, specify which paths should be protected
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public (public files like images, svgs)
-     * - auth (authentication routes like a custom sign-in page if you create one)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico|public|auth).*)",
+    // Only protect these specific paths
+    "/dashboard",
+    "/account",
+    "/settings"
   ],
-}; 
+};
